4f3c6c97580a897267c9210ac2c870b9be561ecd
Carla <csachett@student.scf.edu>
2022-07-25 03:54:48 -0400

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+main.remove();
+const div = document.createElement(div)
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Carla is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
